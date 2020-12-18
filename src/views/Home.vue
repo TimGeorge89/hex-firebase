c114c5f6df4ed1f55810fb54b9621d119e9eb03a
Tim <timdev89@gmail.com>
2020-12-18 15:36:12 -0500

    styling and data binding

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,12 +5,12 @@
         <!-- NAVIGATION -->
         <div class="nav">
           <!-- single button -->
-          <div class="button">
+          <div class="register">
             <button>Register</button>
           </div>
           <!-- single button -->
-          <div class="button">
-            <button>Register</button>
+          <div class="sign-in">
+            <button>Sign In</button>
           </div>
         </div>
         <!-- HEADER -->
@@ -20,17 +20,17 @@
           <p>Create an account and log in to save your favorites!</p>
         </div>
         <!-- COLOR DISPLAY -->
-        <div class="color-display">
+        <div :style="{ background: activeColor }" class="color-display">
           <p>{{ displayedData }}</p>
         </div>
         <!-- BUTTONS -->
         <div class="buttons">
           <!-- single button -->
-          <div class="button">
-            <button>Generate</button>
+          <div @click="randomColor()" class="button">
+            <button >Generate</button>
           </div>
           <!-- single button -->
-          <div class="button">
+          <div @click="addToList()" class="button">
             <button>List</button>
           </div>
           <!-- single button -->
@@ -38,8 +38,17 @@
             <button>Save</button>
           </div>
           <!-- single button -->
-          <div class="button">
+          <div @click="reset()" class="button">
             <button>Reset</button>
+          </div>
+        </div>
+        <!-- COLOR LIST -->
+        <div class="color-list">
+          <h1>Your Color List:</h1>
+          <div v-for="item in list" :key="item" class="list">
+            <ul>
+              <li>{{ item }}</li>
+            </ul>
           </div>
         </div>
       </div>
@@ -48,9 +57,117 @@
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      displayedData: 'your hex will display here',
+      activeColor: '',
+      list: []
+    }
+  },
+  methods: {
+    randomColor() {
+      const randomColor = Math.floor(Math.random()*16777215).toString(16)
+      if (randomColor.length === 6) {
+        this.displayedData = '#' + randomColor
+        this.activeColor = this.displayedData
+      }
+    },
+    addToList() {
+      this.list.push(this.displayedData)
+    },
+    reset() {
+      this.displayedData = 'your hex will display here'
+      this.activeColor = ''
+      this.list = []
+    }
+  }
+}
 </script>
 
 <style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+}
 
+html {
+  height: 100vh;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  background: grey;
+}
+
+button {
+  outline: none;
+  font-size: 18px;
+}
+
+.main-container {
+  background: white;
+  text-align: center;
+  border-radius: 10px;
+  box-shadow: 10px 15px 10px rgba(0, 0, 0, 0.4);
+}
+
+.wrapper {
+  padding: 25px 50px;
+}
+
+// NAVIGATION
+.nav {
+  display: flex;
+  .sign-in {
+    margin-left: auto;
+  }
+}
+
+// HEADER
+.header {
+  h1 {
+    font-size: 45px;
+    padding: 15px 0 10px 0;
+  }
+  p {
+    font-size: 22px;
+  }
+}
+
+// COLOR DISPLAY
+.color-display {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 20px auto;
+  height: 300px;
+  width: 300px;
+  border: 1px solid black;
+}
+
+// BUTTONS
+.buttons {
+  display: flex;
+  justify-content: center;
+  button {
+    margin: 0 10px;
+    width: 80px;
+  }
+}
+
+// COLOR LIST
+.color-list {
+  h1 {
+    margin: 15px 0;
+  }
+  .list {
+    ul {
+      list-style: none;
+    }
+  }
+}
 </style>
