3c1a90c2d73bc47674a83e775b96bd8fbd4deaaf
Tim <timdev89@gmail.com>
2020-12-18 13:39:42 -0500

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="home">
+    <div class="main-container">
+      <div class="wrapper">
+        <!-- NAVIGATION -->
+        <div class="nav">
+          <!-- single button -->
+          <div class="button">
+            <button>Register</button>
+          </div>
+          <!-- single button -->
+          <div class="button">
+            <button>Register</button>
+          </div>
+        </div>
+        <!-- HEADER -->
+        <div class="header">
+          <h1>RANDOM HEX</h1>
+          <p>Generates a random hex value!</p>
+          <p>Create an account and log in to save your favorites!</p>
+        </div>
+        <!-- COLOR DISPLAY -->
+        <div class="color-display">
+          <p>{{ displayedData }}</p>
+        </div>
+        <!-- BUTTONS -->
+        <div class="buttons">
+          <!-- single button -->
+          <div class="button">
+            <button>Generate</button>
+          </div>
+          <!-- single button -->
+          <div class="button">
+            <button>List</button>
+          </div>
+          <!-- single button -->
+          <div class="button">
+            <button>Save</button>
+          </div>
+          <!-- single button -->
+          <div class="button">
+            <button>Reset</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+
+</style>
